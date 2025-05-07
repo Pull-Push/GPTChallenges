ca7a334354785dfe7fd2662fd0f10a246037e1d4
Jeff Sokol <JeffSokolDev@gmail.com>
2025-05-06 20:48:32 -0400

    challenge_1 css styling

--- Challenge_1/challenge_1/src/index.js
+++ Challenge_1/challenge_1/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import JobCard from './JobCard';
 import reportWebVitals from './reportWebVitals';
