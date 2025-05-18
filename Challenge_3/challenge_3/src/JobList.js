bffdf7255e06fe846774f039ef6f7c3a79d14144
Jeff Sokol <JeffSokolDev@gmail.com>
2025-05-18 13:03:03 -0400

    start of filtering

--- Challenge_3/challenge_3/src/JobList.js
+++ Challenge_3/challenge_3/src/JobList.js
@@ -2,17 +2,7 @@ import React, {useState} from 'react';
 import '../src/static/JobCard.css';
 import { jobData } from './data/joblist';
 import JobCard from './JobCard';
-// TODO turn this into a more react version with useState
 
-    // console.log('clicked style', e.target.style)
-    // console.log('text', e.target.innerHTML)
-    // if(e.target.innerHTML === '♡ SAVE JOB ♡' ) {
-    //     e.target.innerHTML = '♥ JOB FAVORITED! ♥'
-    // }else{
-    //     e.target.innerHTML = '♡ SAVE JOB ♡'
-    // }
-
-// TODO Job Card Container + Job Card
 export default function JobList(){
     const [favorites, setFavorites ] = useState([]) //integer array
     const handleFavorite = (jobID) => {
@@ -23,11 +13,45 @@ export default function JobList(){
         );
     };
     let cards = jobData
+    
+    const handleFilter = (e) =>{
+        let filterCards = []
+        let workingFilter = e
+        if(workingFilter.length ===  0){
+            filterCards = cards
+        } else {   
+            for(let x of cards){
+                const values = Object.values(x) //GET ALL VALUES OF CARDS OBJECTS
+                // console.log('values', values)
+                for(let y of values){ //GET INDY CARD VALUES
+                    let stringed = y.toString() //STRING CARD VALUES TO FIX NUMBER AND BOOLEAN ISSUE
+                    // console.log('inner', stringed)
+                    // console.log(typeof stringed)
+                    if(stringed.includes(workingFilter) && (!(filterCards.includes(x)))){ //FILTER CARDS and avoid dupes
+                        // console.log('found one!')
+                        filterCards.push(x) //ADD CARD TO FILTER ARRAY
+                    }
+                }
+            }
+        }
+            console.log('working filter', workingFilter);
+            console.log('filter cards', filterCards);
+    }
+
+    
+
     return(
         <div className='mainDiv'>
+            <div>
+                <label htmlFor="seach">Filter Jobs</label>
+                <input type="text" name="search" id="search" onChange={(e)=> handleFilter(e.target.value)}/>
+            </div>
+            <div className='indyCard'>
+
             { cards.map((card, index) => 
                 <JobCard key={index} card={card} handleFavorite={handleFavorite} favorites={favorites}/>
             )}
+            </div>
         </div>
     )
 }
